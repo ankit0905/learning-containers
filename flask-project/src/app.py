f81163c4109c1a3c952b07ac801a364e799fe7ad
Ankit Anand <ankit0905anand@gmail.com>
2020-06-29 11:47:55 +0530

    Add redis support

--- flask-project/src/app.py
+++ flask-project/src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import redis
 
 app = Flask(__name__)
 
+r = redis.Redis(host='cache', port=6379, db=0)
+
 @app.route('/')
 def index():
-	return render_template("index.html")
+	r.incr('hits', amount=1)
+	hits = int(r.get('hits').decode('utf-8'))
+	return render_template("index.html", hits=hits)
 
 if __name__ == '__main__':
 	app.run(debug=True)
